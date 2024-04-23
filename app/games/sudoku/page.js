7d60add9f6f811600639a6a25dbb3537a5a37d3d
blnche <peltierblanche@gmail.com>
2024-04-23 11:28:57 +0200

    #17 fixed css numberSelected && footer icons

--- app/games/sudoku/page.js
+++ app/games/sudoku/page.js
@@ -31,7 +31,7 @@ export default function Page() {
         newStartingBoard();
     }, []);
     useEffect(() => {
-        removeValue(solvedBoard, 0);
+        removeValue(solvedBoard, 2);
     }, [solvedBoard]);
 
     const shuffle = array => {
@@ -299,13 +299,13 @@ export default function Page() {
                     </div>
                 ))}
             </div>
-            <div className='number-selector space-x-2 my-4 flex justify-center'>
+            <div className='number-selector my-4 grid grid-cols-5'>
                 {Array.from({length: 9}).map((_, index) => (
-                    <div key={index} className={`kbd cursor-pointer ${numberSelected === index + 1 ? 'ring-1 ring-neutral' : ''}`} onClick={() => handleNumberSelect(index+1)}>
+                    <div key={index} className={`m-1 kbd cursor-pointer ${numberSelected === index + 1 ? 'ring-1 ring-neutral' : ''}`} onClick={() => handleNumberSelect(index+1)}>
                         {index +1}
                     </div>
                 ))}
-                <div className={`kbd cursor-pointer ${numberSelected === 0 ? 'ring-1 ring-neutral' : ''}`} onClick={() => handleNumberSelect(0)}>
+                <div className={`m-1 kbd cursor-pointer ${numberSelected === 0 ? 'ring-1 ring-neutral' : ''}`} onClick={() => handleNumberSelect(0)}>
                     <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px">
                         <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"/>
                     </svg>
@@ -313,7 +313,7 @@ export default function Page() {
             </div>
             <button className='btn btn-primary' onClick={validateBoard}>Validate</button>
             {isSuccess && 
-                <div className='badge badge-primary badge-lg'>You've won !</div>
+                <div className='my-2 badge badge-primary badge-lg'>You've won !</div>
                 // <dialog id='my_modal_1'>
                 //     <div className='modal-box'>
                 //         <p>You've won !</p>
